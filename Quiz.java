dd19705600f02d65ac1e801468df56ffcfdca43c
iuriiryndin <georgiy.wolkow@gmail.com>
2022-05-23 17:25:44 +0300

    L08 - Changed behaviour. Moved all logic to Quiz class and Question class. Changed behaviour for input logic and output as well.

--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by iurii on 21.05.2022
  */
@@ -8,8 +9,30 @@ public class Quiz {
     int allCorrectAnswers;
     int allIncorrectAnswers;
 
-    public Quiz(String quizName, Question[] questions) {
+    Quiz(String quizName, Question[] questions) {
         this.quizName = quizName;
         this.questions = questions;
+    }
+
+    public void startQuiz () {
+        L08HomeWork.displayStringToUser("Пожалуйста, введите свое имя.");
+        this.userName = L08HomeWork.getInputString();
+        L08HomeWork.displayStringToUser("Тест: " + this.quizName);
+    }
+
+    public void askQuestions() {
+        for (Question question : this.questions) {
+            boolean result = (question.askQuestion());
+            if (result) {
+                this.allCorrectAnswers++;
+            } else {
+                this.allIncorrectAnswers++;
+            }
+        }
+    }
+
+    public void finishQuiz() {
+        L08HomeWork.displayStringToUser(this.userName + ", Вы прошли тест.");
+        L08HomeWork.displayStringToUser("Ваш результат: правильно " + this.allCorrectAnswers + ", неправильно " + this.allIncorrectAnswers);
     }
 }
